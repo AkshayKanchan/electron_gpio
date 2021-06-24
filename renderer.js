b308714fcbb2aa805e3f9a772c7dcd8211fd53ca
Akshay Kanchan <akshaykanchan07@gmail.com>
2021-06-24 19:00:27 +0530

    Test (#5)
    * Test Commit
    
    Test commit
    
    * github testing
    
    github testing

--- renderer.js
+++ renderer.js
@@ -1,4 +1,7 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+// Must add some meaningfull logic in here
+// Testing something on github
 // Testing something else
